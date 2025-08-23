c10367ce9aa7368dbfc15ae297939f28a4db2ff1
Oleg Mezentsev <oleg@ll.codes>
2025-08-23 18:10:34 +0300

    Add ‘asm_lsp’ Language Server for NASM/GAS/GO Assembly

--- init.lua
+++ init.lua
@@ -591,6 +591,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        asm_lsp = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
